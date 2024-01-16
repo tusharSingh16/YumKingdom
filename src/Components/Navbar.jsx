52694b748c9df420f3710100bbb0aa872987363a
Tushar Singh <tushar@Tushars-MacBook-Pro.local>
2024-01-17 00:15:22 +0530

    title bold

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div style={{marginBottom:40}}>
         <nav className = "navbar navbar-expand-lg bg-body-tertiary">
   <div className = "container-fluid">
-    <a className = "navbar-brand" href="#">TOMATO</a>
+    <a className = "navbar-brand" href="#"><b>YumKingdom</b></a>
     <button className = "navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className = "navbar-toggler-icon"></span>
     </button>
